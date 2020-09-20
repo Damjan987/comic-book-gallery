8f1d0a98ab1301043f7ca4aab1aecb9e24e04666
Damjan Vlaić <damjanvlaic7@gmail.com>
2020-09-20 11:08:00 +0100

    modified comic books controller to use repository

--- Controllers/ComicBooksController.cs
+++ Controllers/ComicBooksController.cs
@@ -3,34 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Comic_Book_Gallery.Data;
 using Comic_Book_Gallery.Models;
 
 namespace Comic_Book_Gallery.Controllers
 {
     public class ComicBooksController : Controller
     {
+        private ComicBookRepository _comicBookRepository = null;
+
+        public ComicBooksController()
+        {
+            _comicBookRepository = new ComicBookRepository();
+        }
+
         // GET: ComicBooks
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult Detail()
+        // ovo upitnik znaci da id parametar moze biti nullable
+        public ActionResult Detail(int? id)
         {
-            var comicBook = new ComicBook()
+            if (id == null)
             {
-                SeriesTitle = "The Amazing Spider-man",
-                IssueNumber = 700,
-                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
-                Artists = new Artist[]
-                {
-                    new Artist() { Name = "Dan Slott", Role = "Script" },
-                    new Artist() { Name = "Humberto Ramos", Role = "Pencils" },
-                    new Artist() { Name = "Victor Olazaba", Role = "Inks" },
-                    new Artist() { Name = "Edgar Delgado", Role = "Colors" },
-                    new Artist() { Name = "Chris Eliopoulos", Role = "Letters" }
-                }
-            };
+                return HttpNotFound();
+            }
+
+            var comicBook = _comicBookRepository.GetComicBook((int)id);
 
             return View(comicBook);
         }
